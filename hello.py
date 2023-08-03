e21726b448a059c5d6d148cb06fff72044a091e7
Anurag Bansode <anurag.bansode@hdfcsec.com>
2023-08-03 12:37:06 +0530

    new

--- hello.py
+++ hello.py
@@ -1,47 +1,10 @@
-# region essentinalImports
-import os
-import sys
 import json
 from flask_cors import CORS, cross_origin
 from flask_restx import Api, Resource, fields
 from flask import request, Flask
 from datetime import date
 
-# endregion
-#!IMP import
-# region mandatory Folder
-sys.path.append(os.getcwd())
-if not os.path.exists(f"DigilockerImages"):
-    os.makedirs(f"DigilockerImages")
-
-if not os.path.exists(f"logs\\{date.today()}"):
-    os.makedirs(f"logs\\{date.today()}")
-
-if not os.path.exists(f"logs\\{date.today()}\\info_{date.today()}.log"):
-    open(f"logs\\{date.today()}\\info_{date.today()}.log", "a+")
-    open(f"logs\\{date.today()}\\error_{date.today()}.log", "a+")
-# endregion
-# region user modules
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# Get the parent directory (project/package_a)
-parent_dir = os.path.dirname(current_dir)
-# Append the parent directory to sys.path
-sys.path.append(parent_dir)
-try:
-    from config import logger
-    from Services.cvl import cvl_main
-    from Services.snorkel import Snorkel_sms, Snorkel_email
-    from Services.DigilockerAadhar import (
-        DigilockerRequestGenerator,
-        DigilockerResponse,
-    )
-    from Services.PennyDrop.PennyDrop import DigioBankVerification
-    from Services.DigioCKYC.CKyc import DigioCKYC
-except Exception as e:
-    print("swagger Module Error", e)
-# endregion
-
-## Start of appilcation
+# Start of appilcation
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:4200"])
 # Create the Flask-RESTx API
@@ -83,7 +46,7 @@ class generate_RID(Resource):
     def post(self):
         response = request.json
         try:
-            name = input_check(response["name"])
+            name = response["name"]
 
             return json.dumps({'name':name})
         except:
